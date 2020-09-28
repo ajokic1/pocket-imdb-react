80444d52858b8df76b4f9ace4c358e42a1210280
Andrej Jokić <aj.jokic@gmail.com>
2020-09-28 12:06:47 +0200

    #PIT3-39 Update MovieService

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,12 +1,12 @@
 import ApiService from './ApiService';
 
 const ENDPOINTS = {
-  MOVIES: '/api/movies'
+  MOVIES: '/api/movies?page=:page'
 };
 
 class MovieService extends ApiService {
   getMovies = (page=1) => {
-    return this.apiClient.get(ENDPOINTS.MOVIES + "?page=" + page);
+    return this.apiClient.get(ENDPOINTS.MOVIES.replace(":page", page));
   };
 }
 
