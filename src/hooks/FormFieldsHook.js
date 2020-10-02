98554c49b0802ad9de8b25ba6c64c17bb333ee66
Andrej Jokić <aj.jokic@gmail.com>
2020-10-02 13:48:02 +0200

    #PIT3-61 Add multiple genres to movies

--- src/hooks/FormFieldsHook.js
+++ src/hooks/FormFieldsHook.js
@@ -20,9 +20,9 @@ function useFormFields(config) {
 
   const changeHandlers = {};
   for (const fieldName in config) {
-    if (config[fieldName].values)
-      changeHandlers[fieldName] = createSelectHandler(fieldName);
-    else changeHandlers[fieldName] = createChangeHandler(fieldName);
+    changeHandlers[fieldName] = config[fieldName].values
+      ? createSelectHandler(fieldName)
+      : createChangeHandler(fieldName);
   }
 
   function createChangeHandler(fieldName) {
@@ -78,13 +78,12 @@ function useFormFields(config) {
     for (const fieldName in formFields) {
       const field = formFields[fieldName];
       if (field.multi && field.value) {
-        values[fieldName] = field.value.map(option => option.value);
+        values[fieldName] = field.value.map((option) => option.value);
       } else if (field.values && field.value) {
         values[fieldName] = field.value.value;
       } else {
         values[fieldName] = field.value;
       }
-      
     }
     return values;
   }
